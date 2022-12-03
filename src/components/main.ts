48dbf115ac17b43d3a9549929ec5f63e1d6c7fc4
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2022-12-03 19:09:02 +0100

    fixes on keyup and down

--- src/components/main.ts
+++ src/components/main.ts
@@ -13,7 +13,7 @@ export class KeyBinder {
     private readonly meta_keys = ['Control', 'Shift', 'Alt', 'Meta', 'ctrl'].map(v => v.toLowerCase());
     private readonly can_shift = 'abcdefghijklmnopqrstuvwxyz1234567890'
     private last_combination :string = ''
-    private timer_lsitner ?: NodeJS.Timeout
+    private timer_listener ?: NodeJS.Timeout
     private just_listened = false
     private main_listener: any
     private timer = 200
@@ -46,8 +46,6 @@ export class KeyBinder {
         return element?.addEventListener(listener, e => this.listenerCallback(e))
     }
     private listenerCallback(e : Event){
-            //check if e is type of KeyboardEvent
-            console.log(this.settings)
             const listener = e.type ?? this.settings.listener_type
             if(!(e instanceof KeyboardEvent)) return
             // let can_shift = !this.settings.case_sensitive ? this.can_shift+this.can_shift.toUpperCase() : this.can_shift
@@ -58,32 +56,44 @@ export class KeyBinder {
             }
             let alphanumeric = this.can_shift+this.can_shift.toUpperCase()
             let key : string|null = e?.key
-            // //// console.log(key)
-            // console.log(e.key)
-            clearTimeout(this.timer_lsitner)
+
+            clearTimeout(this.timer_listener)
+            //if not just listened to clear current stroke
             if(!this.just_listened){
                 this.current_stroke = []
                 this.just_listened = true
             }
+            // add meta key if no duplicate meta keys already present
             e?.ctrlKey && this.current_stroke.indexOf('ctrl') < 0 ? this.current_stroke.push('ctrl') : null
             e?.altKey && this.current_stroke.indexOf('alt') < 0  ? this.current_stroke.push('alt') : null
             e?.metaKey && this.current_stroke.indexOf('meta') < 0  ? this.current_stroke.push('meta') : null
+            // add shift key if no duplicate shift keys already presents and key is part of the alphanumeric keys
             e?.shiftKey && this.current_stroke.indexOf('shift') < 0 && alphanumeric.indexOf(`${e?.key}`) >= 0 && `${e?.key}`.length == 1 ? this.current_stroke.push('shift') : null
+            // because in keypress e.key name changes from `b` to `keyB` so we have to get the proper key name
             if(listener == 'keypress' && e.code.toLowerCase() !== 'enter')  key = `${e?.key}`.length > 1 ? e?.code[e?.code.length - 1] : e?.key
             if(listener != 'keypress' && this.meta_keys.indexOf(`${key}`) >= 0) key = null
             // //// console.log(key)
+
+            //if part of meta key and special function keys let it be set to lowercase 
             if(`${key}`.length > 1) key = `${key}`.toLowerCase()
             // if(['Control', 'Shift', 'Alt'].indexOf(key) > 0)key = null
+
+            //setting space key
             if(e?.key.trim().length == 0) key = 'space'
-            // console.log(e.code, listener)
-            key ? this.current_stroke.push(key) : null
-            //removing the shift key if the key is not part of the alphanumeric characters and character lenght is one
-            if(alphanumeric.indexOf(`${e?.key}`) < 0 && this.current_stroke.filter(v => alphanumeric.indexOf(`${v}`) < 0 && this.meta_keys.indexOf(`${key}`) < 0).length > 1){
-                this.current_stroke = this.current_stroke.filter(v => v.toLowerCase() != 'shift')
+
+            //adding the key to current key stroke 
+            key  ? this.current_stroke.push(key) : null
+
+            // console.log((alphanumeric.indexOf(`${e?.key}`) < 0 && this.current_stroke.filter(v => alphanumeric.indexOf(`${v}`) < 0 && this.meta_keys.indexOf(`${v}`) < 0 && v.length < 2).length == 1))
+            //removing the shift key if the key is not part of the alphanumeric characters and character lenght is one or special function keys like (space, tab)
+            if(alphanumeric.indexOf(`${e?.key}`) < 0 && this.current_stroke.filter(v => alphanumeric.indexOf(`${v}`) < 0 && this.meta_keys.indexOf(`${v}`) < 0 && v.length < 2).length == 1){
+                // console.log('testing')
+                this.current_stroke = this.current_stroke.filter(v => v.toLowerCase() != 'shift'.toLowerCase())
             }
+            // changing any instance of control from e.key to ctrl and also removing any duplicate meta key like shift and ctrl
             this.current_stroke = this.current_stroke.map(v => v == 'control' ? 'ctrl' : v).filter((value, index, self) => this.meta_keys.indexOf(`${value.toLowerCase()}`) >= 0 ? self.findIndex(v => v === value) === index : value)
             this.last_combination = this.current_stroke.join('+')
-            this.timer_lsitner = this.setTimeout(this.timer - 10)
+            this.timer_listener = this.setTimeout(this.timer - 10)
             this.just_listened = true
             // //// console.log(this.current_stroke, can_shift.indexOf(e?.key))
         }
